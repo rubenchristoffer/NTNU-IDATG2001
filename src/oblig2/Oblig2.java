98054370f3f59d5cadf68f110cc28ce90194bbe6
Ruben <rubenc@protonmail.com>
2020-02-07 11:19:20 +0100

    Initialized Oblig2

---
+++ src/oblig2/Oblig2.java
@@ -0,0 +1,13 @@
+package oblig2;
+
+/**
+ * Main class for Obligatory task 2.
+ * @author Ruben Christoffer
+ */
+public final class Oblig2 {
+
+	public static void Main (String[] args) {
+		
+	}
+	
+}
