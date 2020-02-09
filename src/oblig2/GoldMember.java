1d820ed3a4235ac368bc72b691baa83d9a65b120
Ruben <rubenc@protonmail.com>
2020-02-09 21:59:10 +0100

    Added javadoc to GoldMember.

--- src/oblig2/GoldMember.java
+++ src/oblig2/GoldMember.java
@@ -2,15 +2,28 @@ package oblig2;
 
 import java.time.LocalDate;
 
+/**
+ * A Gold Member is a Bonus Member with the highest
+ * amount of privileges where their base points
+ * are multiplied by {@link BonusMember#FACTOR_GOLD}.
+ * @author Ruben Christoffer
+ */
 public class GoldMember extends BonusMember {
 
+	/**
+	 * Initializes a new GoldMember instance.
+	 * @param memberNo is the membership number
+	 * @param personals contains data about a member
+	 * @param enrolledDate is the enrollment date of the member
+	 * @param bonusPoints is the amount of bonus points that this member has previously.
+	 */
 	public GoldMember(int memberNo, Personals personals, LocalDate enrolledDate, int bonusPoints) {
 		super(memberNo, personals, enrolledDate, bonusPoints);
 	}
 
 	@Override
-	public void registerPoints(int bonusPoints) {
-		super.registerPoints((int)(bonusPoints * BonusMember.FACTOR_GOLD));
+	public void registerPoints(int basePoints) {
+		super.registerPoints((int)(basePoints * BonusMember.FACTOR_GOLD));
 	}
 
 }
