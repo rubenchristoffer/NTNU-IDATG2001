e2d04db7b9bef5d4820f0a5ce34caa87b629a2c3
Ruben Christoffer <rubenc@protonmail.com>
2020-03-09 21:42:20 +0100

    Added GoldMember test for checkMembers test

--- src/oblig2/MemberArchiveTest.java
+++ src/oblig2/MemberArchiveTest.java
@@ -12,10 +12,12 @@ public class MemberArchiveTest {
 	private Personals person1;
 	private Personals person2;
 	private Personals person3;
+	private Personals person4;
 	
 	private LocalDate date1;
 	private LocalDate date2;
 	private LocalDate date3;
+	private LocalDate date4;
 	private LocalDate emulatedTodayDate;
 	
 	@BeforeEach
@@ -23,10 +25,12 @@ public class MemberArchiveTest {
 		person1 = new Personals("first", "last", "example@example.com", "pass");
 		person2 = new Personals("first2", "last2", "example2@example.com", "pass");
 		person3 = new Personals("first3", "last3", "example3@example.com", "pass3");
+		person4 = new Personals("first4", "last4", "example4@example.com", "password");
 		
 		date1 = LocalDate.of(2000, 1, 25);
 		date2 = LocalDate.of(1997, 1, 25);
 		date3 = LocalDate.of(1999, 8, 25);
+		date4 = LocalDate.of(1999, 8, 25);
 		emulatedTodayDate = LocalDate.of(2000, 2, 1);
 	}
 	
@@ -35,11 +39,15 @@ public class MemberArchiveTest {
 		System.out.println(String.format("Date1: %s, Date2: %s, Emulated today date: %s", date1, date2, emulatedTodayDate));
 		
 		MemberArchive memberArchive = new MemberArchive();
+		
 		int id1 = memberArchive.addMember(person1, date1);
 		int id2 = memberArchive.addMember(person2, date2);
 		int id3 = memberArchive.addMember(person3, date3);
+		int id4 = memberArchive.addMember(person3, date3);
+		
 		memberArchive.registerPoints(id2, (int)MemberArchive.SILVER_LIMIT + 1);
 		memberArchive.registerPoints(id3, (int)MemberArchive.SILVER_LIMIT + 1);
+		memberArchive.registerPoints(id4, (int)MemberArchive.GOLD_LIMIT);
 		
 		memberArchive.checkMembers(emulatedTodayDate);
 		
@@ -51,6 +59,9 @@ public class MemberArchiveTest {
 		
 		System.out.println("Check if person3 is upgraded to SilverMember (has enough points)");
 		assertEquals(true, (memberArchive.getMember(id3) instanceof SilverMember));
+		
+		System.out.println("Check if person4 is upgraded to GoldMember (has enough points)");
+		assertEquals(true, (memberArchive.getMember(id4) instanceof GoldMember));
 	}
 	
 }
