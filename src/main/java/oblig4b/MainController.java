79de6326009d653e4fc3848b887b2e44997c7e1f
Ruben Christoffer <rubenc@protonmail.com>
2020-03-10 23:09:51 +0100

    Started working on add member dialog

--- src/main/java/oblig4b/MainController.java
+++ src/main/java/oblig4b/MainController.java
@@ -1,11 +1,16 @@
 package oblig4b;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import oblig2.BonusMember;
@@ -17,6 +22,22 @@ public class MainController implements Initializable {
 	private TableView<BonusMember> table;
 	
 	private MemberArchive archive;
+	private Dialog<BonusMember> addMemberDialog;
+	
+	public MainController() {
+		try {
+			addMemberDialog = new Dialog<BonusMember>();
+			addMemberDialog.setTitle("Add new member");//addMemberDialog.setHeaderText("New member ID: 2000")
+			addMemberDialog.onShowingProperty().addListener(event -> System.out.println("hello"));
+			
+			ButtonType loginButtonType = new ButtonType("Login", ButtonData.OK_DONE);
+			addMemberDialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+			
+			addMemberDialog.getDialogPane().setContent(FXMLLoader.load(getClass().getResource("add_member_dialog.fxml")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
@@ -30,11 +51,11 @@ public class MainController implements Initializable {
 		// The next lines get values directly using code
 		table.getColumns().get(3).setCellValueFactory(celldata -> new ReadOnlyObjectWrapper(celldata.getValue().getClass().getSimpleName()));
 		table.getColumns().get(4).setCellValueFactory(celldata -> new ReadOnlyObjectWrapper(celldata.getValue().getPersonals().getSurname()));
-	}
+	}	
 	
 	@FXML
 	private void addMember () {
-		
+		addMemberDialog.showAndWait();
 	}
 	
 	@FXML
