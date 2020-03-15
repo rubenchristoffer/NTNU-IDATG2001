b29c618dea345a879a9f39db018b8acd3e1fb30a
Ruben Christoffer <rubenc@protonmail.com>
2020-03-15 21:51:49 +0100

    New try at JavaFX testing

--- src/test/java/oblig4b/Oblig4BTest.java
+++ src/test/java/oblig4b/Oblig4BTest.java
@@ -1,10 +1,6 @@
 package oblig4b;
 
-import java.util.concurrent.TimeoutException;
-
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationTest;
 
 import javafx.stage.Stage;
@@ -17,17 +13,10 @@ import javafx.stage.Stage;
  */
 public class Oblig4BTest extends ApplicationTest {
 
-	private Oblig4B oblig4b;
-	
 	@Override
 	public void start(Stage stage) throws Exception {
-		oblig4b = new Oblig4B();
+		Oblig4B oblig4b = new Oblig4B();
 		oblig4b.start(stage);
-	}
-	
-	@AfterEach
-	public void tearDown () throws TimeoutException {
-		FxToolkit.hideStage();
 	}
 	
 	@Test
