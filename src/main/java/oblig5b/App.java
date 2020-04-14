b208857f939f94d1d409f96387e4768a9a58662b
Ruben Christoffer <rubenc@protonmail.com>
2020-04-14 18:02:27 +0200

    Made 5b.3 WildAnimalFactory not static

--- src/main/java/oblig5b/App.java
+++ src/main/java/oblig5b/App.java
@@ -5,9 +5,11 @@ import java.time.LocalDate;
 public final class App {
 
 	public static void main (String[] args) {
-		ScandinavianWildAnimal ulla = WildAnimalFactory.newFemaleWolf(LocalDate.of(2015,4,29), "Ulla",  LocalDate.of(2015,2,26), "Innhegning 2, Skandinaviske rovdyr", 2);
-		ScandinavianWildAnimal truls = WildAnimalFactory.newMaleWolf(LocalDate.of(2015,4,29), "Truls",  LocalDate.of(2015,2,26), "Innhegning 2, Skandinaviske rovdyr");
-		ScandinavianWildAnimal bjornar = WildAnimalFactory.newMaleBear(LocalDate.of(2012, 5, 5), "Bjørnar", LocalDate.of(2011, 5, 5), "Innhegning 3, Skandinaviske rovdyr");
+		WildAnimalFactory waf = new WildAnimalFactory();
+		
+		ScandinavianWildAnimal ulla = waf.newFemaleWolf(LocalDate.of(2015,4,29), "Ulla",  LocalDate.of(2015,2,26), "Innhegning 2, Skandinaviske rovdyr", 2);
+		ScandinavianWildAnimal truls = waf.newMaleWolf(LocalDate.of(2015,4,29), "Truls",  LocalDate.of(2015,2,26), "Innhegning 2, Skandinaviske rovdyr");
+		ScandinavianWildAnimal bjornar = waf.newMaleBear(LocalDate.of(2012, 5, 5), "Bjørnar", LocalDate.of(2011, 5, 5), "Innhegning 3, Skandinaviske rovdyr");
 	}
 	
 }
--- src/main/java/oblig5b/WildAnimalFactory.java
+++ src/main/java/oblig5b/WildAnimalFactory.java
@@ -6,19 +6,19 @@ import java.time.LocalDate;
  * Factory class for generating wild animal objects.
  * @author R.C. Hegland-Antonsen
  */
-public final class WildAnimalFactory {
+public class WildAnimalFactory {
 
-	public static ScandinavianWildAnimal newMaleBear(LocalDate arrivalDate, String name,
+	public ScandinavianWildAnimal newMaleBear(LocalDate arrivalDate, String name,
 			LocalDate dateOfBirth, String address) {
 		return new MaleIndividual("Bjørn", "Ursus arctos", "Ursidae", arrivalDate, name, dateOfBirth, true, address);
 	}
 
-	public static ScandinavianWildAnimal newFemaleWolf(LocalDate arrivalDate, String name,
+	public ScandinavianWildAnimal newFemaleWolf(LocalDate arrivalDate, String name,
 			LocalDate dateOfBirth, String address, int noLitters) {
 		return new FemaleIndividual("Ulv", "Canis lupus", "Canidae", arrivalDate, name, dateOfBirth, true, address, noLitters);
 	}
 
-	public static ScandinavianWildAnimal newMaleWolf(LocalDate arrivalDate, String name,
+	public ScandinavianWildAnimal newMaleWolf(LocalDate arrivalDate, String name,
 			LocalDate dateOfBirth, String address) {
 		return new MaleIndividual("Ulv", "Canis lupus", "Canidae", arrivalDate, name, dateOfBirth, true, address);
 	}
