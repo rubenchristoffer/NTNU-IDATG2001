58289e60bb0af731584abba1180743babb116c69
Ruben Christoffer <rubenc@protonmail.com>
2020-04-14 18:11:56 +0200

    Completed oblig5b

--- src/main/java/oblig5b/WildAnimalFactory.java
+++ src/main/java/oblig5b/WildAnimalFactory.java
@@ -4,23 +4,37 @@ import java.time.LocalDate;
 
 /**
  * Factory class for generating wild animal objects.
+ * 
  * @author R.C. Hegland-Antonsen
  */
-public class WildAnimalFactory {
+public final class WildAnimalFactory {
 
-	public ScandinavianWildAnimal newMaleBear(LocalDate arrivalDate, String name,
-			LocalDate dateOfBirth, String address) {
+	private static WildAnimalFactory instance;
+
+	private WildAnimalFactory() {
+	}
+
+	public ScandinavianWildAnimal newMaleBear(LocalDate arrivalDate, String name, LocalDate dateOfBirth,
+			String address) {
 		return new MaleIndividual("Bjørn", "Ursus arctos", "Ursidae", arrivalDate, name, dateOfBirth, true, address);
 	}
 
-	public ScandinavianWildAnimal newFemaleWolf(LocalDate arrivalDate, String name,
-			LocalDate dateOfBirth, String address, int noLitters) {
-		return new FemaleIndividual("Ulv", "Canis lupus", "Canidae", arrivalDate, name, dateOfBirth, true, address, noLitters);
+	public ScandinavianWildAnimal newFemaleWolf(LocalDate arrivalDate, String name, LocalDate dateOfBirth,
+			String address, int noLitters) {
+		return new FemaleIndividual("Ulv", "Canis lupus", "Canidae", arrivalDate, name, dateOfBirth, true, address,
+				noLitters);
 	}
 
-	public ScandinavianWildAnimal newMaleWolf(LocalDate arrivalDate, String name,
-			LocalDate dateOfBirth, String address) {
+	public ScandinavianWildAnimal newMaleWolf(LocalDate arrivalDate, String name, LocalDate dateOfBirth,
+			String address) {
 		return new MaleIndividual("Ulv", "Canis lupus", "Canidae", arrivalDate, name, dateOfBirth, true, address);
+	}
+
+	public static WildAnimalFactory getInstance() {
+		if (instance == null)
+			instance = new WildAnimalFactory();
+
+		return instance;
 	}
 
 }
